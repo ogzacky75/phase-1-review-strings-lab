e7d7c707342efaebcee2271c7ef2a73cfb7f4786
ogzacky75 <ogzacky75@gmail.com>
2025-06-08 19:21:27 +0300

    adding code

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser='Isaac'
+
+let welcomeMessage="Welcome to Flatbook, Isaac!"
+
+let excitedWelcomeMessage="WELCOME TO FLATBOOK, Isaac!"
+
+let shortGreeting="Welcome, I!"
